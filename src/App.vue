d0cc3c0abd94477d278bfa9216639ce62fe8924c
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-02-01 15:28:10 +0100

    Aggiunge chiamata axios e predispone per stampa

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,41 @@
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+
+import axios from "axios";
+import ProjectCard from "./components/ProjectCard.vue";
+
 export default {
   components: {
-    HelloWorld,
+    ProjectCard,
   },
+  data() {
+    return {
+      projects: {},
+    }
+  },
+  created(){
+    axios.get(`http://127.0.0.1:8000/api/projects`)
+      .then(res => {
+        this.projects = res.data;
+        console.log(this.projects.results);
+      });
+  }
+
 };
+
 </script>
 
 <template>
+
   <div>
-    <HelloWorld />
+
+    <ProjectCard />
+
   </div>
+
 </template>
 
 <style lang="scss">
+
 @import "./style/global.scss";
+
 </style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,21 @@
+<script>
+
+export default {
+  name: "ProjectCard",
+};
+
+</script>
+
+<template>
+
+  <div>
+    <h1>ProjectCard</h1>
+  </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+
+
+</style>
